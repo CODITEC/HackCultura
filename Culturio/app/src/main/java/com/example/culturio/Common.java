a1a7a9d7da0508b64d038c488c0bce9ab643da7d
francobko <franco.condor.urp@gmail.com>
2019-06-15 18:47:25 -0500

    MARKERS

---
+++ Culturio/app/src/main/java/com/example/culturio/Common.java
@@ -0,0 +1,7 @@
+package com.example.culturio;
+
+import android.location.Location;
+
+public class Common {
+    public static Location mLastLocation = null;
+}
--- Culturio/app/src/main/java/com/example/culturio/CulturioActivity.java
+++ Culturio/app/src/main/java/com/example/culturio/CulturioActivity.java
@@ -1,8 +1,15 @@
 package com.example.culturio;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,10 +21,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.example.culturio.Common;
 
 public class CulturioActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
@@ -25,7 +44,20 @@ public class CulturioActivity extends AppCompatActivity
     private GoogleMap mMap;
     SupportMapFragment mapFragment;
 
-    Marker culturePlayer;
+    Marker culturePlayer, ministerioCultura;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationCallback locationCallback;
+
+    //PLAY SERVICE
+    private static final int MY_PERMISSION_REQUEST_CODE = 7192;
+    private static final int PLAY_SERVICE_RES_REQUEST = 300193;
+
+    private LocationRequest mLocationRequest;
+
+    private static int UPDATE_INTERVAL = 5000;
+    private static int FATEST_INTERVAL = 3000;
+    private static int DISPLACEMENT = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +71,88 @@ public class CulturioActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map_home);
         mapFragment.getMapAsync(this);
         navigationView.setNavigationItemSelectedListener(this);
+        setUpLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setUpLocation();
+                }
+                break;
+        }
+    }
+
+    private void setUpLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //SOLICITUD DE RUNTIME PERMISO
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, MY_PERMISSION_REQUEST_CODE);
+        } else {
+            if (ActivityCompat.checkSelfPermission(CulturioActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(CulturioActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            buildLocationCallBack();
+            buildLocationRequest();
+            fusedLocationProviderClient.requestLocationUpdates(mLocationRequest, locationCallback, Looper.myLooper());
+
+        }
+    }
+
+    private void buildLocationCallBack() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Common.mLastLocation = locationResult.getLocations().get(locationResult.getLocations().size() - 1);
+                displayLocation();
+            }
+        };
+    }
+
+    private void displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                Common.mLastLocation = location;
+                if (Common.mLastLocation != null) {
+                    //AGREGAR MARCADOR
+                    if (culturePlayer != null)
+                        culturePlayer.remove(); // Quitar marcadores que ya existan
+                    culturePlayer = mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(Common.mLastLocation.getLatitude(), Common.mLastLocation.getLongitude()))
+                            .title(String.format("Tú"))
+                            .flat(true)
+                            .icon(BitmapDescriptorFactory.fromResource((R.drawable.searcher))));
+
+                    // Mover camara
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Common.mLastLocation.getLatitude(), Common.mLastLocation.getLongitude()), 15.0f));
+                } else {
+                    Log.d("ERROR", "No se pudo obtener tu ubicación");
+                }
+                //fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+            }
+        });
+    }
+
+    private void buildLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
     }
 
     @Override
@@ -111,5 +220,23 @@ public class CulturioActivity extends AppCompatActivity
         mMap.setIndoorEnabled(false);
         mMap.setBuildingsEnabled(false);
         mMap.getUiSettings().setZoomControlsEnabled(false);
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                return false;
+            }
+        });
+        //mMap.getUiSettings().setZoomGesturesEnabled(false);
+
+        ministerioCultura = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(-12.086101,-77.001915))
+                .title(String.format("Museo"))
+                .flat(true)
+                .icon(BitmapDescriptorFactory.fromResource((R.drawable.museum_sign))));
+        ministerioCultura.showInfoWindow();
+
+
+
     }
 }
