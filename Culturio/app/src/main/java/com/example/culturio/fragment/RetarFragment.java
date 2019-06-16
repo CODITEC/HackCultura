3e6d10e84bc9f05180142aa028d76a7e6bedf4ae
AbelTarazona <abel.tarazona@urp.edu.pe>
2019-06-15 22:13:08 -0500

    retar fragment

---
+++ Culturio/app/src/main/java/com/example/culturio/fragment/RetarFragment.java
@@ -0,0 +1,32 @@
+package com.example.culturio.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.culturio.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RetarFragment extends Fragment {
+
+
+    public RetarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_retar, container, false);
+
+        return view;
+    }
+
+}
