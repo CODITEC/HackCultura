8bba604b7e53d7d27d27401fd9274b5c301fdc42
francobko <franco.condor.urp@gmail.com>
2019-06-16 00:34:03 -0500

    FINALIZAR JUEGO

--- Culturio/app/src/main/java/com/example/culturio/Common.java
+++ Culturio/app/src/main/java/com/example/culturio/Common.java
@@ -10,8 +10,8 @@ public class Common {
 
     public static final HashMap<Integer, String> listaPreguntas = new HashMap<Integer, String>();
     static {
-        listaPreguntas.put(1,"¿Hasta finales de que período existieron los ammonites?|Jurásico|Triásico|Cretásico|3");
-        listaPreguntas.put(2,"¿Qué cultura se caracterizaba por sus cerámicas modeladas con variedad de forma decorativa?|Moche|Paracas caverna|Inca|2");
-        listaPreguntas.put(3,"¿Qué cultura se desarrolló entre los valles de Fortaleza, Pativilca, Supe?|Cultura Chancay|Cultura Tiahuanaco|Cultura Lima|1");
+        listaPreguntas.put(1,"¿Hasta finales de que período existieron los ammonites?-Jurásico-Triásico-Cretásico-3-colmillo-Cretásico-Fósil de ammonite entero y semipulido");
+        listaPreguntas.put(2,"¿Qué cultura se caracterizaba por sus cerámicas modeladas con variedad de forma decorativa?-Moche-Paracas caverna-Inca-2-paracas-Paracas caverna-Botella de cuerpo globular achatado y asa puente");
+        listaPreguntas.put(3,"¿Qué cultura se desarrolló entre los valles de Fortaleza, Pativilca, Supe Huaura,Chancay entre los años 1200 y 1470 d.c.?-Cultura Chancay-Cultura Tiahuanaco-Cultura Lima-1-canino-Cultura Chancay-Vasija escultórica que representa un ser zoomorfo");
     }
 }
--- Culturio/app/src/main/java/com/example/culturio/fragment/QuizFragment.java
+++ Culturio/app/src/main/java/com/example/culturio/fragment/QuizFragment.java
@@ -1,7 +1,9 @@
 package com.example.culturio.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,6 +18,7 @@ import com.example.culturio.R;
 public class QuizFragment extends Fragment {
 
     ImageView respuestaA, respuestaB, respuestaC;
+    TextView txtQuestion, txtRespuestaA, txtRespuestaB, txtRespuestaC;
 
     public QuizFragment() {
         // Required empty public constructor
@@ -31,30 +34,56 @@ public class QuizFragment extends Fragment {
         respuestaB = view.findViewById(R.id.respuestaB);
         respuestaC = view.findViewById(R.id.respuestaC);
 
+        Log.d("QUIZ", ""+Common.positionQuiz);
         Log.d("QUIZ", ""+Common.listaPreguntas.get(Common.positionQuiz));
+        String quiz = Common.listaPreguntas.get(Common.positionQuiz);
+        final String[] split = quiz.split("-");
+
+        Log.d("QUIZ", ""+split);
+        txtQuestion = view.findViewById(R.id.txtQuestion);
+        txtRespuestaA = view.findViewById(R.id.txtRespuestaA);
+        txtRespuestaB = view.findViewById(R.id.txtRespuestaB);
+        txtRespuestaC = view.findViewById(R.id.txtRespuestaC);
+
+        txtQuestion.setText(split[0]);
+        txtRespuestaA.setText(split[1]);
+        txtRespuestaB.setText(split[2]);
+        txtRespuestaC.setText(split[3]);
 
         respuestaA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialogAnswer(0);
+                Integer answer = 0;
+                if(split[4].equals("1")){
+                    answer = 1;
+                }
+                showDialogAnswer(answer,split[5],split[6],split[7]);
             }
         });
         respuestaB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialogAnswer(0);
+                Integer answer = 0;
+                if(split[4].equals("2")){
+                    answer = 1;
+                }
+                showDialogAnswer(answer,split[5],split[6],split[7]);
             }
         });
         respuestaC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialogAnswer(1);
+                Integer answer = 0;
+                if(split[4].equals("3")){
+                    answer = 1;
+                }
+                showDialogAnswer(answer,split[5],split[6],split[7]);
             }
         });
         return view;
     }
 
-    private void showDialogAnswer(int i) {
+    private void showDialogAnswer(int i, String s1, String s2, String s3) {
         final AlertDialog.Builder dialog = new AlertDialog.Builder(getContext());
         dialog.setTitle("RESULTADO: ");
 
@@ -62,20 +91,45 @@ public class QuizFragment extends Fragment {
         View login_layout = inflater.inflate(R.layout.layout_answer, null);
         ImageView imgResultado = login_layout.findViewById(R.id.imgResultado);
         TextView txtResultado = login_layout.findViewById(R.id.txtResultado);
+        TextView txtDescripcion = login_layout.findViewById(R.id.txtDescripcion);
+        ImageView imgAnswer = login_layout.findViewById(R.id.imgAnswer);
+        txtDescripcion.setText(s3);
+        int id = getResources().getIdentifier(s1, "drawable",getContext().getPackageName());
+
+        imgAnswer.setImageResource(id);
+        dialog.setMessage("Respuesta correcta: "+s2);
         if(i == 1){
-            dialog.setMessage("");
             imgResultado.setImageResource(R.drawable.ic_check);
             txtResultado.setText("¡RESPUESTA CORRECTA!");
         } else {
-            dialog.setMessage("Respuesta correcta: A) TUMI");
             imgResultado.setImageResource(R.drawable.ic_cancel);
             txtResultado.setText("¡RESPUESTA INCORRECTA!");
         }
-        /*
-        final MaterialEditText edtUsuario = login_layout.findViewById(R.id.edtUsuario);
-        final MaterialEditText edtPass = login_layout.findViewById(R.id.edtPassword);
-        */
+
         dialog.setView(login_layout);
+        String textoBotton = "SIGUIENTE";
+        if(Common.positionQuiz == 3){
+            textoBotton = "FINALIZAR";
+        }
+        dialog.setNegativeButton(textoBotton, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(Common.positionQuiz == 3){
+                    cargarFramento(new ResultadoFragment());
+                } else {
+                    cargarFramento(new QuizFragment());
+                }
+            }
+        });
         dialog.show();
+    }
+
+    private void cargarFramento(Fragment fragment){
+        /*Bundle bundle = new Bundle();
+        bundle.putString("id",id);
+        fragment.setArguments(bundle);*/
+
+        FragmentManager manager = getFragmentManager();
+        manager.beginTransaction().replace(R.id.map_home,fragment).addToBackStack(null).commit();
     }
 }
