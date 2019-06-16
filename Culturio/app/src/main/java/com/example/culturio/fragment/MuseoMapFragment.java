29dd52332887c9bcb546c735724ff0948b20bc78
francobko <franco.condor.urp@gmail.com>
2019-06-15 21:18:18 -0500

    QUIZ DISEÑO

--- Culturio/app/src/main/java/com/example/culturio/fragment/MuseoMapFragment.java
+++ Culturio/app/src/main/java/com/example/culturio/fragment/MuseoMapFragment.java
@@ -5,9 +5,11 @@ import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.culturio.R;
 
@@ -18,6 +20,7 @@ import java.util.Locale;
  */
 public class MuseoMapFragment extends Fragment {
 
+    Button btnStart;
 
     public MuseoMapFragment() {
         // Required empty public constructor
@@ -29,9 +32,25 @@ public class MuseoMapFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_museo_map, container, false);
+        btnStart = view.findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarFramento(new QuizFragment());
+            }
+        });
 
 
         return view;
+    }
+
+    private void cargarFramento(Fragment fragment){
+        /*Bundle bundle = new Bundle();
+        bundle.putString("id",id);
+        fragment.setArguments(bundle);*/
+
+        FragmentManager manager = getFragmentManager();
+        manager.beginTransaction().replace(R.id.map_home,fragment).addToBackStack(null).commit();
     }
 
 }
